60819a149b23f9d141db7acb311f04e2bcc3fe29
mlellis5 <mlelli5@emory.edu>
2024-04-28 19:07:59 -0400

    first

---
+++ R/logLikBernoulli.R
@@ -0,0 +1,32 @@
+#' Calculate the parameter p that maximizes the log-likelihood of a Bernoulli distribution
+#'
+#' This function takes a vector of binary outcomes and performs a grid-based search to find
+#' the parameter p that maximizes the log-likelihood of observing the data under a Bernoulli
+#' distribution.
+#'
+#' @param data A vector of binary outcomes (0 and 1).
+#' @return The p value that maximizes and the log-likelihood of the vector of binary outcomes.
+#' @export
+#'
+#' @examples
+#' data = c(1, 0, 0, 0, 1, 1, 1)
+#' logLikBernoulli(data)
+#'
+#' @importFrom stats seq log sum which.max
+
+logLikBernoulli = function(data) {
+  # Grid-based search for parameter p in steps of 0.001
+  pvals = seq(0, 1, by = 0.001)
+  LL = numeric(length(pvals))
+  
+  # Calculate log-likelihood for each p value
+  for (i in seq_along(pvals)) {
+    p = pvals[i]
+    LL[i] = sum(data * log(p) + (1 - data) * log(1 - p))
+  }
+  
+  # Find the maximum log-likelihood and corresponding p value
+  maxLL = max(LL)
+  LLp = pvals[which.max(LL)]
+  return(LLp)
+}
